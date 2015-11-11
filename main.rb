06fad5e27471b3281fd50c74e3136174195687fd
beersheep <beersheep0109@gmail.com>
2015-11-11 15:43:55 +0800

    added hit & stay button

--- main.rb
+++ main.rb
@@ -7,19 +7,37 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'your_secret' 
 
 helpers do 
-  def add_up_total
+
+  def add_up_total(cards)
+    card_value = cards.map{|card| card[1]}
+    total = 0
+
+    card_value.each do |value|
+      if value == "A"
+        total += 11
+      else
+        total += value.to_i == 0 ? 10 : value.to_i
+      end
+    end
+
+    card_value.select {|value| value == "A"}.count.times do
+      break if total <= 21
+      total -= 10 
+    end
+    total
   end
+
+
 end
 
 get '/' do
-  erb :set_name
   if session[:player_name]
     redirect '/game'
   end
+  erb :set_name
 end
 
 post '/set_name' do
-  # binding.pry
   session[:player_name] = params[:name]
   redirect '/game'
 end
@@ -27,11 +45,44 @@ end
 get '/bet' do
 end
 
+post '/set_bet' do
+end
+
 get '/game' do
+  if !session[:player_name]
+    redirect '/'
+  end
   value = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-  suit = %w(Spade Heart Diamond Club)
-  session[:deck] = suit.product(value)
+  suit = %w(spade heart diamond club)
+  session[:deck] = suit.product(value).shuffle!
   session[:player_cards] = []
-  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] = []
+  2.times do 
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+  end
   erb :game
 end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if add_up_total(session[:player_cards]) > 21
+    @error = "#{session[:player_name]} busted! Dealer wins!"
+  end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} chose to stay."
+
+  erb :game
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+
+end
+
+
+
